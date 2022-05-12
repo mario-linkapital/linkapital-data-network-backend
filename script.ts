81032ea2de7188b9508d4ab2e4714ca93d0586c4
Mario Olivares <mario@linkapital.com.br>
2022-05-12 18:38:31 -0400

    Empresas de un municipio

--- script.ts
+++ script.ts
@@ -8,11 +8,11 @@ async function main() {
     // SELECT municipio, count(cnpj_basico) as total from public.estabelecimento where uf = 'AC' group by municipio
 
     /*const estabelecimento: object | null = await prisma.estabelecimento.findMany({
-        take: 2,
+        take: 2
         where: {
             uf: 'AC',
         },
-        select: {
+        include: {
             _count: {
                 select: {
                     municipio: true,
@@ -21,7 +21,7 @@ async function main() {
             },
         }
     })*/
-    const estabelecimento = await prisma.estabelecimento.groupBy({
+    /*const estabelecimento = await prisma.estabelecimento.groupBy({
         by: ['municipio'],
         where: {
             uf: 'AC',
@@ -29,9 +29,29 @@ async function main() {
         _count: {
             cnpj_basico: true,
         },
-    })
+    })*/
+    /*const empresasByMunicipio = await prisma.estabelecimento.groupBy({
+        by: ['municipio'],
+        where: {
+            uf: 'AC',
+        },
+        _count: {
+            cnpj_basico: true,
+        },
+    })*/
+
+    const results = await prisma.empresa.findMany({
+        take: 1,
+        where: {
+            estabels: {
+                some: {
+                    municipio: "0001",
+                }
+            }
+        }
+    });
     // use `console.dir` to print nested objects
-    console.dir(estabelecimento, { depth: null })
+    console.dir(results, { depth: null })
 }
 
 main()
